b6e01e737aa39b06d2528e8cedcf3d4f51cade84
Saijal Shakya <saijalshakya1@gmail.com>
2019-01-18 18:10:57 +0545

    problem with generic.ListView

--- polls/urls.py
+++ polls/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # ex: /polls/5/
     path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
@@ -12,3 +13,4 @@ urlpatterns = [
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,20 @@
-from django.http import HttpResponse, Http404
-from .models import Questions
-from django.template import loader
+from django.http import HttpResponseRedirect
+# from django.views.generic import ListView
+
+from .models import Questions, Choice
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
 
-def index(request):
-    latest_question_list = Questions.objects.order_by('pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {'latest_question_list': latest_question_list}
-    return HttpResponse(template.render(context, request))
+def IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Questions.objects.order_by('-pub_date')[:5]
 
 
 def detail(request, question_id):
@@ -24,9 +30,29 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Questions, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Questions, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(
+                reverse(
+                    'polls:results',
+                    args=(question.id,)
+                )
+        )
